b199a3c1317d773639dd22b0cd7fce862c3accc2
Ediezzle <edmore.mandikiyana@gmail.com>
2019-12-29 19:04:21 +0200

    once-off commit

---
+++ GenericClass/MyGenericClass.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericClass
+{
+    public class MyGenericClass<T>
+    { 
+        //Generic variables 
+        private T d1; 
+        private T d2; 
+        private T d3;
+        public MyGenericClass() 
+        {
+            
+        }
+
+        //Generic non-default constructor 
+        public MyGenericClass(T a1, T a2, T a3) 
+        { 
+            d1 = a1; 
+            d2 = a2; 
+            d3 = a3; 
+        }
+
+        //Generic properties 
+        public T D1 
+        {
+            get { return d1; } 
+            set { d1 = value; } 
+        }
+        public T D2 
+        { 
+            get { return d2; } 
+            set { d2 = value; } 
+        }
+        public T D3 
+        { 
+            get { return d3; } 
+            set { d3 = value; } 
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"D1={d1}, D2={d2}, D3={d3}");
+        }
+        public void ResetValues()
+        {
+            d1 = default(T);
+            d2 = default(T);
+            d3 = default(T);
+        }
+    }
+}
---
+++ GenericClass/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+
+namespace GenericClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //contains int values 
+            MyGenericClass<int> c1 = new MyGenericClass<int>(5, 10, 15);
+
+            //retrieve value for D!
+            Console.WriteLine(c1.D1);
+
+            //display the contents of c1
+            c1.Print();
+
+            //reset values to 0 ()default for int) 
+            c1.ResetValues();
+
+            //display the contents of c1
+            c1.Print();
+
+            //contains string values 
+            MyGenericClass<string> c2 = new MyGenericClass<string>("a", "b", "c");
+
+            //display the contents of c2 
+            c2.Print();
+
+            //reset values to null (default for strings)
+            c2.ResetValues();
+
+            //display the contents of c2
+            c2.Print();
+        }
+    }
+}
